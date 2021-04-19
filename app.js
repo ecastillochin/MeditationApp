80a4ac077621ac115828725a2783d5e24b8beeb6
rcastilloc <rcastilloc@frd.org.mx>
2021-04-19 14:40:32 -0500

    Added functionality
    selector song, validation playing, update time on display

--- app.js
+++ app.js
@@ -4,11 +4,69 @@ const app = () => {
     const outline = document.querySelector('.moving-outline circle');
     const video = document.querySelector('.vid-container video');
 
+    const timeSelect = document.querySelectorAll(".time-select button");
+
     const sounds = document.querySelectorAll('.sound-picker button');
     const timeDisplay = document.querySelector('.time-display');
     const outlinelength = outline.getTotalLength();
 
-    console.log(outlinelength);
+    let fakeDuration =600;
+
+    outline.style.strokeDasharray = outlinelength;
+    outline.style.strokeDashoffset = outlinelength;
+
+    sounds.forEach(sound => {
+        sound.addEventListener("click", function(){
+            song.src = this.getAttribute("data-sound");
+            video.src = this.getAttribute("data-video");
+            checkPlaying(song);
+        })
+    });
+    
+    
+    play.addEventListener("click",() =>{
+        checkPlaying(song);
+    });
+
+    const checkPlaying = song =>{
+        if (song.paused){
+            song.play();
+            video.play();
+            play.src='./svg/pause.svg';
+        }
+        else{
+            song.pause();
+            video.pause();
+            play.src='./svg/play.svg';
+        }
+    }
+
+    timeSelect.forEach(option =>{
+        option.addEventListener("click", function(){
+            fakeDuration = this.getAttribute("data-time");
+            timeDisplay.textContent=`${Math.floor(fakeDuration/60)}:${Math.floor(fakeDuration%60)}`
+        })
+    })
+
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        let progress = outlinelength - (currentTime /fakeDuration) * outlinelength;
+        outline.style.strokeDashoffset= progress;
+
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if (currentTime >= fakeDuration){
+            song.pause();
+            video.pause();
+            song.currentTime = 0;
+            play.src = "./svg/play.svg"
+        }
+    }
+
 
 }
 
